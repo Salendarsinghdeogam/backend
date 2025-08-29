b51ea453f0e9032c7c06dce4bf826678fe946382
salendar singh <105363282+Salendarsinghdeogam@users.noreply.github.com>
2025-08-29 11:26:58 +0530

    middle wire creation

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-<<<<<<< HEAD
 const express = require ('express')
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.use(function(req,res,next){
+  console.log("mmiddle wire working");
+  next();
 })
-app.get('/profile', (req, res) => {
-  res.send('mera profile')
+app.use(function(req,res,next){
+  console.log("mmiddle wire worked 2nd time");
+  next();
+})
+
+app.get("/",(req,res)=>{
+  res.send("hello mountains v");
+})
+app.get("/about",(req,res)=>{
+  res.send("hello terrains");
 })
 
 app.listen(3001)
-=======
-const http = require ('http');
-
-const server= http.createServer (function (paramreq, res) {
-    res.end("hello world");
-    
-})
-server.listen(3001);
->>>>>>> 5ec84eaaa885d05922000989d653537931555ea8
